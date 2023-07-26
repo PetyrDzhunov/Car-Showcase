bee70d667019e5042217fdf4dc4c9b11399da07f
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2023-07-26 12:11:35 +0300

    Hero and CustomButton

--- components/index.ts
+++ components/index.ts
@@ -1,5 +1,6 @@
 // This file will export all of our components so we can import them anywhere in a simpler way
 
 import Hero from './Hero';
+import CustomButton from './CustomButton';
 
-export { Hero };
+export { Hero, CustomButton };

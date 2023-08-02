9e0ec030d38ae41f6fd955941214e8c78543cd54
petardzhunov <petar.djunov@abv.bg>
2023-08-02 14:41:03 +0300

    Show pictures dynamically from imagin studio API

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { CarCardComponents } from '@/types';
 import CustomButton from './CustomButton';
-import { calculateCarRent } from '@/utils';
+import { calculateCarRent, generateCarImageUrl } from '@/utils';
 import CarDetails from './CarDetails';
 
 const CarCard = ({ car }: CarCardComponents) => {
@@ -26,7 +26,7 @@ const CarCard = ({ car }: CarCardComponents) => {
       </p>
       <div className="relative w-full h-40 my-3 object-contain">
         <Image
-          src="/hero.png"
+          src={generateCarImageUrl(car)}
           alt="car model"
           fill
           priority
--- utils/index.ts
+++ utils/index.ts
@@ -1,5 +1,7 @@
+import { Car } from "@/types";
+
 const baseUrl = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars'
-const url = `${baseUrl}/?model=corolla`;
+const url = `${baseUrl}/?model=q3`;
 
 export async function fetchCars() {
     const headers = {
@@ -28,3 +30,20 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
     return rentalRatePerDay.toFixed(0);
 };
+
+
+export const generateCarImageUrl = (car: Car, angle?: string) => {
+    const url = new URL("https://cdn.imagin.studio/getimage");
+
+    const { make, year, model } = car;
+
+    url.searchParams.append('customer', 'hrjavascript-mastery');
+    url.searchParams.append("make", make);
+    url.searchParams.append("modelFamily", model.split(" ")[0]);
+    url.searchParams.append("zoomType", 'fullscreen');
+    url.searchParams.append("modelYear", `${year}`);
+    url.searchParams.append("angle", `${angle}`);
+
+    return `${url}`
+
+}
